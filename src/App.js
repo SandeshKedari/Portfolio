15f7b0f4ffbbf9e93b25cb7249257da6d0655300
Sandesh <sandeshkedari17@gmail.com>
2023-10-12 11:49:21 +0530

    Page.jsx added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import Page from './Sections/Page';
 import './App.css';
 
 function App() {
   return (
     <div className="App-header">
+    <Page/>
       <h1>Portfolio Starting...</h1>
       <h2>HELLOooooooo</h2>
     </div>
---
+++ src/Sections/Page.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <>
+            <h4>Navbar section</h4>
+            <h4>Titlebar section</h4>
+            <h4>About section</h4>
+            <h4>TechnicalSkills section</h4>
+            <h4>Experience section</h4>
+            <h4>Timeline section</h4>
+            {/* <Navbar />
+            <TitleBar /> */}
+            {/* <About/>
+        <TechnicalSkills/>
+        <Experience/> */}
+            {/* <Project/> */}
+            {/* <Timeline/> */}
+        </>
+    );
+};
+
+export default Page;
