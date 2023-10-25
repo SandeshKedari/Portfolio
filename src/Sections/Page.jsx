e17615043b17f8fad1eb2ea83b9c5b3fcd8088c8
Sandesh <sandeshkedari17@gmail.com>
2023-10-25 23:57:52 +0530

    changes commited for TechnicalSkills.jsx

--- src/Sections/Page.jsx
+++ src/Sections/Page.jsx
@@ -4,10 +4,12 @@ import About from './About';
 import TechnicalSkills from './TechnicalSkills';
 import Experience from './Experience';
 import TimeLine from './TimeLine';
+
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const Page = () => {
     return (
+        
         <>
-            
             <Titlebar />
             <About/>
             <TechnicalSkills/>
@@ -16,12 +18,27 @@ const Page = () => {
             <div className="footer m-1">
             <TimeLine/>
             </div>
-           
-           
-                 
-            
+
+
+
+
         </>
+
     );
 };
 
 export default Page;
+
+// <Router>
+//             <Titlebar />
+//             <Routes>
+//                 <Route path="/" element={<About />} />
+
+//                 <Route path="technicalskill" element={<TechnicalSkills />} />
+//                 <Route path="experience" element={<Experience />} />
+//                 <Route path="downloadcv" element={<About />} />
+//                 <Route path="footer" element={<TimeLine />} />
+                 
+
+//             </Routes>
+//         </Router>
--- src/Sections/TechnicalSkills.jsx
+++ src/Sections/TechnicalSkills.jsx
@@ -1,11 +1,80 @@
 import React from 'react';
+import SkillCards from './SkillCards';
 
 const TechnicalSkills = () => {
     return (
-        <div>
-            <h1>TechnicalSkills section</h1>
-        </div>
+        <>
+            <h1>What I do</h1>
+            <h5>Below is the quick overview of my technical skill sets and technology i use.
+                Want to find more about my experience ? Checkout my Resume & Work Experience</h5>
+            <div className="container" >
+                <div className="row" >
+
+                    <div className="col">
+                        <SkillCards name='welcome to Page'
+                            title="ReactJS"
+                        />
+                    </div>
+
+                    <div className="col">
+                        <SkillCards name='welcome to Page'
+                            title="HTML"
+                        />
+                    </div>
+
+                    <div className="col">
+                        <SkillCards name='welcome to Page'
+                            title="CSS"
+                        />
+                    </div>
+                    <div className="col">
+                        <SkillCards name='welcome to Page'
+                            title="JavaScript"
+                        />
+                    </div>
+                    <div className="col">
+                        <SkillCards name='welcome to Page'
+                            title="Bootstrap"
+                        />
+                    </div>
+
+                    <div className="col">
+                        <SkillCards name='welcome to Page'
+                            title="NodeJS"
+                        />
+                    </div>
+
+                </div>
+            </div>
+        </>
     );
 };
 
 export default TechnicalSkills;
+
+
+// <div className="container" >
+// <div className="row" >
+
+//     <div className="col">
+//         <Card name='welcome to About Page'
+//             imgsrc="Aster.jpg"
+//             title="Aster Plant"
+//             price="₹ 40"
+//             desc='/Aster' />
+//     </div>
+//     <div className="col">
+//         <Card name='welcome to About Page'
+//             imgsrc="Dahila.jpg"
+//             title="Dahila Plant" price="₹ 40" />
+
+//     </div>
+
+
+//     <div className="col">
+//         <Card name='welcome to About Page'
+//             imgsrc="JatrophaRed.jpg"
+//             title="Jatropha Red Plant" price="₹ 40" />
+//     </div>
+// </div>
+// </div>
